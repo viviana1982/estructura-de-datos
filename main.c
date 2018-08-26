9c6df10e15c864c328c92c6eaeef91c08e36b452
viviana1982 <42543354+viviana1982@users.noreply.github.com>
2018-08-26 13:57:17 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,53 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void pedirDatos();
-int contVocales(char*);
-
-char dato[30];
-
 int main()
 {
-    pedirDatos();
-    contVocales(dato);
+    int a, *p;
+    p = &a;
+    system("cls");
+    printf("ingrese un valor para la variable:\n");
+    scanf("%d",&a);
 
+    while(a>=47 && a<=57){
+        printf("error, el valor debe ser mayor que cero:\n");
+        scanf("%d",&a);
+    }
+    system("cls");
+    printf("a %d\n",a);
+    printf("la direccion es %d\n",&a);
+    printf("*p=%p\n", p);
+    printf("a=%d\n", *p);
+    printf("el tamaño de *p es %d\n", sizeof(p));
+//    delete[] p;
+    printf("el valor de p es: %d\n", p);
+    p= NULL;
+    printf("valor de p: %d\n", p);
+    getch();
     return 0;
 }
-
-void pedirDatos(){
-
-    printf("ingrese una cadena de caracteres\n");
-    fgets(dato,30,stdin);
-
-}
-
-int contVocales(char *cadena){
-    int cont=0, cont_a=0, cont_e=0, cont_i=0, cont_o=0, cont_u=0 ;
-    while(*cadena){
-        switch(*cadena){
-        case 'a': cont_a ++;
-        break;
-        case 'e':cont_e ++;
-        break;
-        case 'i': cont_i ++;
-        break;
-        case 'o': cont_o ++;
-        break;
-        case 'u': cont_u ++;
-        break;
-
-        }
-        cadena ++;
-    }
-    printf("la cantidad de a es: %d\n", cont_a);
-    printf("la cantidad de e es: %d\n", cont_e);
-    printf("la cantidad de i es: %d\n", cont_i);
-    printf("la cantidad de o es: %d\n", cont_o);
-    printf("la cantidad de u es: %d\n", cont_u);
-    printf("la direccion de memoria: %p\n", cadena);
-
-
-}
-
-
-
